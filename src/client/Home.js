3d0351fa0bfb08ac7124d0a954be69b2a56f34bd
Andrew <andrewgamble.ca@gmail.com>
2020-02-22 22:09:00 -0800

    Updated Creative section

--- src/client/Home.js
+++ src/client/Home.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import Logo from './Logo';
+import React, { Component } from "react";
+import Logo from "./Logo";
 
-import me from './images/meSmall.jpg';
-import instaLogo from './images/instaLogo.png';
-import wordpressLogo from './images/wordpressLogo.png';
-import youtubeLogo from './images/youtubePlayLogo.png';
-import linkedinLogo from './images/linkedinLogo.png';
-import githubLogo from './images/githubLogo.png';
+import me from "./images/meSmall.jpg";
+import instaLogo from "./images/instaLogo.png";
+import wordpressLogo from "./images/wordpressLogo.png";
+import youtubeLogo from "./images/youtubePlayLogo.png";
+import linkedinLogo from "./images/linkedinLogo.png";
+import githubLogo from "./images/githubLogo.png";
 
 const logos = [
-  { photo: instaLogo, url: 'https://www.instagram.com/andrew_gamble' },
-  { photo: wordpressLogo, url: 'https://andrewgamblee.wordpress.com/' },
-  { photo: youtubeLogo, url: 'https://www.youtube.com/channel/UCdUc2TaPzSjy1xDlQtFGo4Q' },
-  { photo: linkedinLogo, url: 'https://www.linkedin.com/in/andrew-gamble-20900bb1/' },
-  { photo: githubLogo, url: 'https://github.com/gamblea/' }];
+  { photo: instaLogo, url: "https://www.instagram.com/andrew_gamble" },
+  { photo: wordpressLogo, url: "https://andrewgamblee.wordpress.com/" },
+  {
+    photo: youtubeLogo,
+    url: "https://www.youtube.com/channel/UCdUc2TaPzSjy1xDlQtFGo4Q"
+  },
+  {
+    photo: linkedinLogo,
+    url: "https://www.linkedin.com/in/andrew-gamble-20900bb1/"
+  },
+  { photo: githubLogo, url: "https://github.com/gamblea/" }
+];
 
 export default function Home() {
   return (
@@ -25,27 +32,38 @@ export default function Home() {
         <div className="col-md align-self-center">
           <div className="mx-auto ">
             <p>
-          Hi, I’m Andrew. I am an undergraduate student at the University of Waterloo, who enjoys eating pineapple on his pizza. I am eager learner and openly enjoy the process of expanding my understanding, either in the classroom or on my own.
+              Hi, I’m Andrew. I am an undergraduate student at the University of
+              Waterloo, who enjoys eating pineapple on his pizza. I am eager
+              learner and openly enjoy the process of expanding my
+              understanding, either in the classroom or on my own.
             </p>
             <p>
-         My main interests academically are in mathematics and computer science along with the hardware side of computing. Outside of academics I love being active, coding, reading about math, cooking, playing volleyball and sharing my experiences through making videos and photography.
+              My main interests academically are in mathematics and computer
+              science along with the hardware side of computing. Outside of
+              academics I love being active, coding, reading about math,
+              cooking, playing volleyball and sharing my experiences through
+              making videos and photography.
             </p>
             <p>
-         Being from the west coast I love getting outside and being active. From skiing through the trees to a long day of hiking there is nothing better than exploring the outdoors. In addition, I love experimenting in the kitchen and trying out new ways to not follow the recipe.
+              Being from the west coast I love getting outside and being active.
+              From skiing through the trees to a long day of hiking there is
+              nothing better than exploring the outdoors. In addition, I love
+              experimenting in the kitchen and trying out new ways to not follow
+              the recipe.
             </p>
             <p>
-         Feel free to reach out, I’m always interested to meet new awesome people!
+              Feel free to reach out, I’m always interested to meet new awesome
+              people!
             </p>
-            <p>
-         Andrew
-            </p>
+            <p>Andrew</p>
           </div>
         </div>
       </div>
       <div className="row logos">
-        {logos.map((logo, index) => <Logo {...logo} key={index} />)}
+        {logos.map((logo, index) => (
+          <Logo {...logo} key={index} />
+        ))}
       </div>
     </div>
-
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-const outputDirectory = 'docs';
+const outputDirectory = "docs";
 
 module.exports = {
-  entry: ['babel-polyfill', './src/client/index.js'],
+  entry: ["babel-polyfill", "./src/client/index.js"],
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
   module: {
     rules: [
@@ -16,16 +16,16 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       },
       {
         test: /\.(s?)css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
+        loader: "url-loader?limit=100000"
       }
     ]
   },
@@ -33,14 +33,14 @@ module.exports = {
     port: 3000,
     open: true,
     proxy: {
-      '/api': 'http://localhost:8080'
+      "/api": "http://localhost:8080"
     }
   },
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.ico'
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico"
     })
   ]
 };
